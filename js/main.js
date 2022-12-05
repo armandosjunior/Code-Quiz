d8ee5a266923d94361c538e6b3325d96c7421503
Armando S <107372777+armandosjunior@users.noreply.github.com>
2022-12-05 05:42:33 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,173 @@
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
+
+// if startQuiz button clicked
+start_btn.onclick = ()=> {
+    info_box.classList.add("activeInfo"); //vissible
+}
+
+// if exitQuiz button clicked
+exit_btn.onclick = ()=> {
+    info_box.classList.remove("activeInfo"); //hide info box
+}
+
+// if continueQuiz button clicked
+continue_btn.onclick = ()=> {
+    exit_btn.style.visibility = "hidden";
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    showQuetions(0);
+    startTimer(30); 
+    startTimerLine(0);
+}
+
+let timeValue =  30;
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+// if restartQuiz button clicked
+restart_quiz.onclick = ()=> {
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    result_box.classList.remove("activeResult"); //hide result box
+    timeValue = 30; 
+    que_count = 0;
+    que_numb = 1;
+    userScore = 0;
+    widthValue = 0;
+    showQuetions(que_count);
+    startTimerLine(widthValue); //calling startTimerLine function
+    timeText.textContent = "Time Left"; //change the text of timeText to Time Left
+    next_btn.classList.remove("show"); //hide the next button
+}
+
+// if quitQuiz button clicked
+quit_quiz.onclick = ()=>{
+    window.location.reload(); //reload the current window
+}
+
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
+
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){ 
+       que_count++;
+       que_numb++;
+        showQuetions(que_count); //calling showQestions function
+        timeText.textContent = "Time Left"; //change the timeText to Time Left
+        next_btn.classList.remove("show");
+    }else{
+       showResult(); //calling showResult function
+    }
+}
+
+// getting questions and options from array
+function showQuetions(index){
+    const que_text = document.querySelector(".que_text");
+
+    //creating a new span and div tag for question and option and passing the value using array index
+    let que_tag = '<span>'+ questions[index].number + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    que_text.innerHTML = que_tag; //adding new span tag inside que_tag
+    option_list.innerHTML = option_tag; //adds div tag inside option_tag
+    
+    const option = option_list.querySelectorAll(".option");
+
+    // set onclick attribute to all available options
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+//if user clicked on option
+function optionSelected(answer){
+    let userAns = answer.textContent; //getting user selected option
+    let correcAns = questions[que_count].answer; //getting correct answer from array
+    const allOptions = option_list.children.length; //getting all option items
+    
+    if(userAns == correcAns){ //if user selected option is equal to array's correct answer
+        userScore += 1; //adding one digit number to score
+        answer.classList.add("correct"); //adding green color to correct answers
+        console.log("Correct Answer");
+        console.log("Your correct answers = " + userScore);
+    }else{
+        answer.classList.add("incorrect");
+        console.log("Wrong Answer");
+
+        for(i=0; i < allOptions; i++){
+            if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer 
+                option_list.children[i].setAttribute("class", "option correct");
+                console.log("Auto selected correct answer.");
+            }
+        }
+    }
+    for(i=0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+    }
+    next_btn.classList.add("show"); //show the next button if user selected any option
+
+}
+
+function showResult(){
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.remove("activeQuiz"); //hide quiz box
+    result_box.classList.add("activeResult"); //show result box
+    const scoreText = result_box.querySelector(".score_text");
+    if (userScore > 3){ // if user scored more than 3
+        let scoreTag = '<span> Congrats, You scored '+ userScore +' of '+ questions.length +'. </span>';
+        scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
+    }
+    else if(userScore > 1){ // if user scored more than 1
+        let scoreTag = '<span> You got '+ userScore +' of '+ questions.length +'.</span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else{ // if user scored less than 1
+        let scoreTag = '<span> Yikes. You scored '+ userScore +' out of '+ questions.length +'.</span>';
+        scoreText.innerHTML = scoreTag;
+    }
+}
+
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time; //changing the value of timeCount with time value
+        time--; //decrement the time
+      
+        if(time < 0){ //if timer is less than 0
+            clearInterval(counter); //clears counter
+            timeText.textContent = "Stop.";
+            const allOptions = option_list.children.length; //getting all option items
+            let correcAns = questions[que_count].answer; //getting correct answer from array
+            for(i=0; i < allOptions; i++){
+                if(option_list.children[i].textContent == correcAns){ 
+                    option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
+                }
+            }
+           showResult()
+        }
+    }
+}
+
+function startTimerLine(time){
+    counterLine = setInterval(timer, 55);
+    function timer(){
+        time ++;
+    }
+}
---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+  number: 1,
+  question: "Commonly used data-types DO Not include:",
+  answer: "alerts",
+  options: [
+    "alerts",
+    "strings",
+    "booleans",
+    "numbers"
+  ]
+},
+  {
+  number: 2,
+  question: "The condition in an if / else statement is enclosed with _____.",
+  answer: "curly brackets",
+  options: [
+    "curly bracket",
+    "quotes",
+    "parenthesis",
+    "square brackets"
+  ]
+},
+  {
+  number: 3,
+  question: "Arrays in JavaScript can be used to store _____.",
+  answer: "all of the above",
+  options: [
+    "numbers and strings",
+    "boolean",
+    "other arrays",
+    "all of the above"
+  ]
+},
+  {
+  number: 4,
+  question: "String values must be enclosed within _____ when being assigned to variables.",
+  answer: "qoutes",
+  options: [
+    "commas",
+    "qoutes",
+    "curly bracket",
+    "parenthesis"
+  ]
+},
+  {
+  number: 5,
+  question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+  answer: "console.log",
+  options: [
+    "console.log",
+    "Javascirpt",
+    "for loops",
+    "terminal/bash"
+  ]
+},
+];
